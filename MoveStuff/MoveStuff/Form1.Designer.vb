1d9ab1259dee9e62458f2923d16296f7380a3e8d
DeltaDizzy <EJBraswell@gmail.com>
2019-09-25 10:38:11 -0500

    Add files via upload

---
+++ MoveStuff/MoveStuff/Form1.Designer.vb
@@ -0,0 +1,57 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pbBox = New System.Windows.Forms.PictureBox()
+        CType(Me.pbBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pbBox
+        '
+        Me.pbBox.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.pbBox.Image = Global.MoveStuff.My.Resources.Resources.Bronze_metal_blue_icon
+        Me.pbBox.Location = New System.Drawing.Point(12, 12)
+        Me.pbBox.Name = "pbBox"
+        Me.pbBox.Size = New System.Drawing.Size(50, 50)
+        Me.pbBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pbBox.TabIndex = 0
+        Me.pbBox.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.ClientSize = New System.Drawing.Size(562, 191)
+        Me.Controls.Add(Me.pbBox)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.KeyPreview = True
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.pbBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pbBox As PictureBox
+End Class
---
+++ MoveStuff/MoveStuff/Form1.vb
@@ -0,0 +1,59 @@
+﻿Public Class Form1
+    Dim wasd = True
+    Const speed = 5
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.Escape Then
+            End
+        End If
+
+        If wasd Then
+            If e.KeyCode = Keys.W Then
+                pbBox.Top -= speed
+            End If
+            If e.KeyCode = Keys.A Then
+                pbBox.Left -= speed
+            End If
+            If e.KeyCode = Keys.S Then
+                pbBox.Top += speed
+            End If
+            If e.KeyCode = Keys.D Then
+                pbBox.Left += speed
+            End If
+        Else
+            If e.KeyCode = Keys.Up Then
+                pbBox.Top -= speed
+            End If
+            If e.KeyCode = Keys.Left Then
+                pbBox.Left -= speed
+            End If
+            If e.KeyCode = Keys.Down Then
+                pbBox.Top += speed
+            End If
+            If e.KeyCode = Keys.Right Then
+                pbBox.Left += speed
+            End If
+        End If
+        BoundsCheck()
+    End Sub
+
+    Private Sub BoundsCheck()
+        If pbBox.Location.Y < 0 - pbBox.Height / 2 Then ' top
+            ' MsgBox("hehehe")
+            pbBox.Location = New Point(pbBox.Location.X, Me.Height - pbBox.Height / 2)
+        End If
+        If pbBox.Location.Y > Me.Height - pbBox.Height / 2 Then ' bottom
+            ' MsgBox("hehehe")
+            pbBox.Location = New Point(pbBox.Location.X, 0 - pbBox.Height / 2)
+        End If
+
+        If pbBox.Location.X < 0 - pbBox.Width / 2 Then ' left
+            ' MsgBox("hehehe")
+            pbBox.Location = New Point(Me.Width - pbBox.Width / 2, pbBox.Location.Y)
+        End If
+
+        If pbBox.Location.X > Me.Width - pbBox.Width / 2 Then ' right
+            ' MsgBox("hehehe")
+            pbBox.Location = New Point(0 - pbBox.Width / 2, pbBox.Location.Y)
+        End If
+    End Sub
+End Class
