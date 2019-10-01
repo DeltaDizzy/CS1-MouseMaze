5ba9d1afeb6f82bd69b42e881299e370ce76cd00
DeltaDizzy <EJBraswell@gmail.com>
2019-10-01 11:05:11 -0500

    Board Game 1.0

---
+++ BoardGame/Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.pbGamePeg = New System.Windows.Forms.PictureBox()
+        Me.pbDice = New System.Windows.Forms.PictureBox()
+        Me.tmrMover = New System.Windows.Forms.Timer(Me.components)
+        Me.pbCow = New System.Windows.Forms.PictureBox()
+        CType(Me.pbGamePeg, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbDice, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbCow, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pbGamePeg
+        '
+        Me.pbGamePeg.BackColor = System.Drawing.Color.Transparent
+        Me.pbGamePeg.Image = Global.BoardGame.My.Resources.Resources.gamepeg
+        Me.pbGamePeg.Location = New System.Drawing.Point(400, 400)
+        Me.pbGamePeg.Name = "pbGamePeg"
+        Me.pbGamePeg.Size = New System.Drawing.Size(100, 100)
+        Me.pbGamePeg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pbGamePeg.TabIndex = 0
+        Me.pbGamePeg.TabStop = False
+        '
+        'pbDice
+        '
+        Me.pbDice.BackColor = System.Drawing.Color.Transparent
+        Me.pbDice.Image = Global.BoardGame.My.Resources.Resources.dice2
+        Me.pbDice.Location = New System.Drawing.Point(176, 171)
+        Me.pbDice.Name = "pbDice"
+        Me.pbDice.Size = New System.Drawing.Size(138, 147)
+        Me.pbDice.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pbDice.TabIndex = 1
+        Me.pbDice.TabStop = False
+        '
+        'tmrMover
+        '
+        Me.tmrMover.Enabled = True
+        Me.tmrMover.Interval = 300
+        '
+        'pbCow
+        '
+        Me.pbCow.BackColor = System.Drawing.Color.Transparent
+        Me.pbCow.Image = Global.BoardGame.My.Resources.Resources.whammy
+        Me.pbCow.Location = New System.Drawing.Point(200, 0)
+        Me.pbCow.Name = "pbCow"
+        Me.pbCow.Size = New System.Drawing.Size(100, 100)
+        Me.pbCow.TabIndex = 2
+        Me.pbCow.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.BoardGame.My.Resources.Resources.game_board
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.ClientSize = New System.Drawing.Size(500, 500)
+        Me.Controls.Add(Me.pbGamePeg)
+        Me.Controls.Add(Me.pbCow)
+        Me.Controls.Add(Me.pbDice)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.pbGamePeg, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbDice, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbCow, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pbGamePeg As PictureBox
+    Friend WithEvents pbDice As PictureBox
+    Friend WithEvents tmrMover As Timer
+    Friend WithEvents pbCow As PictureBox
+End Class
---
+++ BoardGame/Form1.vb
@@ -0,0 +1,72 @@
+﻿Public Class Form1
+    Dim roll As Integer
+    Dim whammycount As Integer = 3
+    Private Sub PbDice_Click(sender As Object, e As EventArgs) Handles pbDice.Click
+        Randomize()
+        roll = Int(Rnd() * 6) + 1
+        ChangeDice()
+        tmrMover.Start()
+    End Sub
+
+    Private Sub ChangeDice()
+        Select Case roll
+            Case 1
+                pbDice.Image = My.Resources.dice1
+            Case 2
+                pbDice.Image = My.Resources.dice2
+            Case 3
+                pbDice.Image = My.Resources.dice3
+            Case 4
+                pbDice.Image = My.Resources.dice4
+            Case 5
+                pbDice.Image = My.Resources.dice5
+            Case 6
+                pbDice.Image = My.Resources.dice6
+        End Select
+    End Sub
+
+    Private Sub MovePeg()
+        ' pbGamePeg.Top -= 100*dice <= TOTAL MOVEMENT 
+        ' only move 100 per timer tick
+        If pbGamePeg.Bounds.IntersectsWith(pbCow.Bounds) And roll = 0 Then
+            Whammy()
+        End If
+        whammycount = 3
+        If roll > 0 Then
+            If pbGamePeg.Location.X = 400 And pbGamePeg.Location.Y > 0 Then ' on right side, not at top
+                pbGamePeg.Top -= 100
+            ElseIf pbGamePeg.Location.X > 0 And pbGamePeg.Location.Y = 0 Then ' on top, not at left
+                pbGamePeg.Left -= 100
+            ElseIf pbGamePeg.Location.X = 0 And pbGamePeg.Location.Y < 400 Then ' on left, not at bottom
+                pbGamePeg.Top += 100
+            ElseIf pbGamePeg.Location.X < 400 And pbGamePeg.Location.Y = 400 Then ' on bottom, not at right
+                pbGamePeg.Left += 100
+            Else
+                tmrMover.Stop()
+            End If
+            roll -= 1
+            My.Computer.Audio.Play(My.Resources.Blop, AudioPlayMode.Background)
+        End If
+    End Sub
+
+    Private Sub TmrMover_Tick(sender As Object, e As EventArgs) Handles tmrMover.Tick
+        MovePeg()
+    End Sub
+    Private Sub Whammy()
+
+        pbGamePeg.Location = New Point(400, 100)
+        'If whammycount > 0 Then
+        '    If pbGamePeg.Location.X = 400 And pbGamePeg.Location.Y > 0 Then ' on right side, not at top
+        '        pbGamePeg.Top += 100
+        '    ElseIf pbGamePeg.Location.X > 0 And pbGamePeg.Location.Y = 0 Then ' on top, not at left
+        '        pbGamePeg.Left += 100
+        '    ElseIf pbGamePeg.Location.X = 0 And pbGamePeg.Location.Y < 400 Then ' on left, not at bottom
+        '        pbGamePeg.Top -= 100
+        '    ElseIf pbGamePeg.Location.X < 400 And pbGamePeg.Location.Y = 400 Then ' on bottom, not at right
+        '        pbGamePeg.Left -= 100
+        '    End If
+        '    whammycount -= 1
+        'End If
+        My.Computer.Audio.Play(My.Resources.wbrb, AudioPlayMode.Background)
+    End Sub
+End Class
